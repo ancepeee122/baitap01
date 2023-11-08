5452272562d19ac984da968755a3571ac12769a6
ancepee122 <icandeptrai@gmail.com>
2023-11-08 16:06:56 +0700

    slide

--- PHP/index.php
+++ PHP/index.php
@@ -3,17 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dinh The Vinh 2121050412</title>
 </head>
 <body>
-    <form action="7-CalResult.php" method="get">
-        <p> First Number: </p>
-        <input type="number" name="firstNumber"><br>
-        <p> Second Number: </p>
-        <input type="number" name="secondNumbe"><br>
+    <form action="5-formGet.php" method="Get">
+        <p>name: </p>
+        <input type="text" name="name" id="name">
+        <p>e - mail :</p>
+        <input type="text" name="email" id="email"><br>
         <p></p>
-        <input type="Submit" value="ADD">
+        <input type="submit">
     </form>
-
 </body>
 </html>
