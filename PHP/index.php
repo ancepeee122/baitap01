7ca4a2feb261e60a23c00e43f74ad65929f0306f
ancepee122 <icandeptrai@gmail.com>
2023-11-08 15:28:46 +0700

    slide

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,17 @@
-<?php
-echo"my first php file";
-echo"hello world";
-echo"<select>";
-echo"<option>1</option>";
-echo"<option>2</option>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dinh The Vinh 2121050412  </title>
+</head>
+<body>
+    <?php
+    echo"Dinh The Vinh 2121050412";
+    echo"hello world";
+    echo"<select>";
+    echo"<option>1</option>";
+    echo"<option>2</option>";
+    ?>
+</body>
+</html>
