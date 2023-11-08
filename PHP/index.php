248a4732dbf23bd45e86884829b257e802413eda
ancepee122 <icandeptrai@gmail.com>
2023-11-08 16:10:36 +0700

    slide

--- PHP/index.php
+++ PHP/index.php
@@ -2,17 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dinh The Vinh 2121050412</title>
+    <title>Document</title>
 </head>
 <body>
-    <form action="5-formGet.php" method="Get">
-        <p>name: </p>
-        <input type="text" name="name" id="name">
-        <p>e - mail :</p>
-        <input type="text" name="email" id="email"><br>
-        <p></p>
-        <input type="submit">
-    </form>
+Welcome <?php echo $_GET["name"]; ?> <br>
+Your email address is : <?php echo $_GET['email'];
 </body>
 </html>
