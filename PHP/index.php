d5de0088545dcf3475971a7f1f3abc6617d301de
ancepee122 <icandeptrai@gmail.com>
2023-11-08 15:32:45 +0700

    slide

--- PHP/index.php
+++ PHP/index.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dinh The Vinh 2121050412  </title>
+    <title>trương đình văn 2121051389</title>
 </head>
 <body>
     <?php
     echo"Dinh The Vinh 2121050412";
-    echo"hello world";
-    echo"<select>";
-    echo"<option>1</option>";
-    echo"<option>2</option>";
     ?>
+    <h3>hello the world</h3>
+    <select name="" id="">
+        <option value="">1</option>
+        <option value="">2</option>
+    </select>
 </body>
 </html>
