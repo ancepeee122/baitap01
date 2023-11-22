f11bdf30d4266a357651555ff4f5203e7bb91063
ancepee122 <icandeptrai@gmail.com>
2023-11-22 14:57:50 +0700

    'slide39'

--- PHP/index.php
+++ PHP/index.php
@@ -2,16 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
 </head>
-<body>
- <form action= "5-formGet.php" method="get">
-    <p> Name : </p>
-    <input type = "text " name = "name "> <br>
-    <p> E-mail : </p>
-    <input type = " text " name = "email"><br>
-    <p></p>
-    <input type= "submit">
-</form>
-</body>
+   <body> 
+        <form action="5-formGet.php" method="get">
+           <p>Name: </p>
+           <input type="text" name="name"><br>
+           <p>E-mail: </p>
+           <input type="text" name="email"><br>
+           <p></p>
+           <input type="submit">
+   </body>
 </html>
