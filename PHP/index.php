01ff116b55ec6616dca29df135bc2fdc1879c83e
ancepee122 <icandeptrai@gmail.com>
2023-11-22 15:26:16 +0700

    slide 18

--- PHP/index.php
+++ PHP/index.php
@@ -6,15 +6,28 @@
     <title></title>
 </head>
    <body> 
-    <h3> QUIT THE GAMER?</h3>
-   <from acction="8-ifResult.php" method="get">
-       <selec name ="response">
-          <option value="yes">YES</option>
-          <option value="no">NO</option>
-       </select>
-       <input type="submit" value="Submit">
-   </form>
-</html>
+    <?php
+     // befor if statement
+     $USD =22300;
+     $EUR =27300;
+     $SGD =17000;
+     $JYP =120;
+     $amount =$_GET['amount'];
+     echo " $amount USD is equal ";
+     if ($_GET["currency"] == "USD") // if-1 statement; condition: $GET["response"] == "yes"
+     {
+      echo $amount * $USD; // if-1 code block
+     }
+     elseif ($_GET["currency"]== "SGD") {
+       echo $amount * $SGD; //eleif-2 code block
+     }
+     else 
+     {
+      echo $amount * $JPY; // else code block 
+     }
+//after if statement
+     echo "VND";
+    ?>
    </body>
 </html>
-<!-- DinhTheVinh 2121050412  -->
+<!-- DinhTheVinh 2121050412 -->
