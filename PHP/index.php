716ff11b965b69bc6f7e2aa811f4c7186526b343
ancepee122 <icandeptrai@gmail.com>
2023-12-05 19:20:58 +0700

    'slide40.5.12.2023'

--- PHP/index.php
+++ PHP/index.php
@@ -1,33 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-   <body> 
-    <?php
-     // befor if statement
-     $USD =22300;
-     $EUR =27300;
-     $SGD =17000;
-     $JYP =120;
-     $amount =$_GET['amount'];
-     echo " $amount USD is equal ";
-     if ($_GET["currency"] == "USD") // if-1 statement; condition: $GET["response"] == "yes"
-     {
-      echo $amount * $USD; // if-1 code block
-     }
-     elseif ($_GET["currency"]== "SGD") {
-       echo $amount * $SGD; //eleif-2 code block
-     }
-     else 
-     {
-      echo $amount * $JPY; // else code block 
-     }
-//after if statement
-     echo "VND";
-    ?>
-   </body>
-</html>
-<!-- DinhTheVinh 2121050412 -->
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'myDB';
+$conn = new mysqli($servername, $username, $password, $db);
+if ($conn->connection_error)
+{
+  die('Connection failed:' . $conn->connection_error);
+
+}
+echo "Connected successfully <br>";
+?>
