70da292081335cbf4818924cb7283b78d6a725a5
ancepee122 <icandeptrai@gmail.com>
2023-12-07 22:33:36 +0700

    'slide58'

--- PHP/index.php
+++ PHP/index.php
@@ -1,22 +1,7 @@
 <?php
 require "connect.php";
-$sql = "SELECT * FROM flights";
+$spl = "SELECT * FROM flights";
 $result = $conn->query($sql);
-if ($result->num_rows > 0)
-{
-  while($row = $result->fecth_assoc())
-
-  {
-    echo "FlightID:" . $row["id"].
-         "Origin :" . $row["origin"].
-         "Destination:" . $row["destination"].
-         "Duration:" . $row["duration"]. "<br>";
-
-  }
-}
-else {
-  echo "No flight in database";
-
-}
+print_r($result);
 $conn->close();
 ?>
