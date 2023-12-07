1f0381054ede2f40173b8130e9014611e87c88e1
ancepee122 <icandeptrai@gmail.com>
2023-12-07 22:46:46 +0700

    slide65

--- PHP/index.php
+++ PHP/index.php
@@ -1,13 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" herf="4-search.css">
+
+</head>
+<body>
+  <h1> SEARCH FLIGHT </h1>
+  <form method="get" action="" name="register">
+    Origin <input type="text" name="origin"/><br>
+    Destination <input type="text" name="destination"/><br>
+    <input type ="submit" value="Search"/> <br>
+</form>
 <?php
-require "connect.php";
-$spl = "SELECT * FROM flights";
-$result = $conn->query($sql);
-print_r($result);
-$row = $result->fetch_assoc();
-echo "<br>"
-print_r($row)
-$row = $result->fetch_assoc();
-echo "<br>";
-print_r($row);
-$conn->close();
+if (isset($_GET['origin']))
+{
+  $origin = $_GET['origin'];
+  $destination = $_GET['destination'];
+  require 'connect.php';
+  mysql_set_charset($conn, 'UTF8');
+  $sql = "SELECT * FORM flights WHERE origin = '$origin' and destination = '$destination'";
+  $result  = $conn->query($sql);
+  require('4-displayFlight.php');
+  $conn->close();
+}
 ?>
+</body>
+</html>
